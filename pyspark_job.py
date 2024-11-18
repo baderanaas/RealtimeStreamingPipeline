629865bb710c1ece17735c37437fadaab6064b51
Anas Bader <anas.bader@insat.ucar.tn>
2024-11-18 21:51:24 +0100

    still having the same problem

--- pyspark_job.py
+++ pyspark_job.py
@@ -1,44 +1,25 @@
 import os
 import logging
-from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Set up logging
 logging.basicConfig(level=logging.INFO)
-
-sfOptions = {
-    "sfURL": f"https://{os.getenv('SNOWFLAKE_ACCOUNT')}.snowflakecomputing.com",
-    "sfUser": os.getenv("SNOWFLAKE_USER"),
-    "sfPassword": os.getenv("SNOWFLAKE_PASSWORD"),
-    "sfDatabase": os.getenv("SNOWFLAKE_DATABASE"),
-    "sfSchema": os.getenv("SNOWFLAKE_SCHEMA"),
-    "sfWarehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
-    "sfRole": os.getenv("SNOWFLAKE_ROLE"),
-}
-
-
-def insert_data(session, **kwargs):
-    logging.info("Inserting data...")
-    try:
-        session.sql(
-            f"INSERT INTO {os.getenv('SNOWFLAKE_DATABASE')}.{os.getenv('SNOWFLAKE_SCHEMA')}.{os.getenv('SNOWFLAKE_TABLE')} "
-            f"(appointment_id, patient_id, patient_name, gender, age, address, symptoms, diagnosis, treatment_plan, appointment_date, doctor_name, hospital, insurance_provider, payment_method) "
-            f"VALUES ('{kwargs['appointment_id']}', '{kwargs['patient_id']}', '{kwargs['patient_name']}', '{kwargs['gender']}', {kwargs['age']}, '{kwargs['address']}', '{kwargs['symptoms']}', '{kwargs['diagnosis']}', '{kwargs['treatment_plan']}', '{kwargs['appointment_date']}', '{kwargs['doctor_name']}', '{kwargs['hospital']}', '{kwargs['insurance_provider']}', '{kwargs['payment_method']}')"
-        )
-        logging.info(
-            f"Data inserted successfully for appointment_id: {kwargs['appointment_id']}"
-        )
-    except Exception as e:
-        logging.error(f"Failed to insert data: {e}")
 
 
 def create_spark_connection():
+    conf = SparkConf()
+    conf.set("spark.master", "spark://spark-master:7077")
+
     try:
-        spark_conn = SparkSession.builder.appName("pyspark-job").getOrCreate()
+        spark_conn = (
+            SparkSession.builder.appName("pyspark-job").config(conf=conf).getOrCreate()
+        )
+        spark_conn.sparkContext.setLogLevel("ERROR")
         logging.info("Spark session created successfully")
         return spark_conn
     except Exception as e:
@@ -46,16 +27,16 @@ def create_spark_connection():
         return None
 
 
-def connect_to_kafka(spark_conn):
+def get_streaming_dataframe(spark_conn):
     try:
         spark_df = (
             spark_conn.readStream.format("kafka")
-            .option("kafka.bootstrap.servers", "localhost:9094")
+            .option("kafka.bootstrap.servers", "broker-debezium:9094")
             .option("subscribe", "cdc.public.appointments")
             .option("startingOffsets", "earliest")
             .load()
         )
-        spark_df.printSchema()
+
         logging.info("Kafka dataframe created successfully")
         return spark_df
     except Exception as e:
@@ -63,12 +44,7 @@ def connect_to_kafka(spark_conn):
         return None
 
 
-def create_snowflake_connection(spark_conn):
-    session = spark_conn.read.format("snowflake").options(**sfOptions).load()
-    return session
-
-
-def create_selection_df_from_kafka(spark_df):
+def transform_streaming_data(df):
     schema = StructType(
         [
             StructField("appointment_id", StringType(), True),
@@ -89,7 +65,7 @@ def create_selection_df_from_kafka(spark_df):
     )
 
     sel = (
-        spark_df.selectExpr("CAST(value AS STRING)")
+        df.selectExpr("CAST(value AS STRING)")
         .select(from_json(col("value"), schema).alias("data"))
         .select("data.*")
     )
@@ -98,34 +74,43 @@ def create_selection_df_from_kafka(spark_df):
     return sel
 
 
+def write_to_file(df: DataFrame, output_path: str):
+    """
+    Writes the processed streaming data to a file.
+
+    :param df: The DataFrame to be written.
+    :param output_path: The directory where the data will be written.
+    """
+    try:
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+            logging.info(f"Created output directory: {output_path}")
+        query = (
+            df.writeStream.format(
+                "json"
+            )  # Use "json" or "csv" depending on your requirement
+            .outputMode("append")  # Options: append, complete, update
+            .option("path", output_path)  # Path to save the output files
+            .option(
+                "checkpointLocation", f"{output_path}/checkpoint"
+            )  # Directory for checkpointing
+            .start()
+        )
+        logging.info(f"Streaming query started, writing to file at {output_path}.")
+        query.awaitTermination()
+    except Exception as e:
+        logging.error(f"Failed to write streaming data to file: {e}")
+
+
+# Main streaming logic
 if __name__ == "__main__":
     spark_conn = create_spark_connection()
 
-    if spark_conn is not None:
-        spark_df = connect_to_kafka(spark_conn)
+    if spark_conn:
+        df = get_streaming_dataframe(spark_conn)
 
-        if spark_df is not None:
-            selection_df = create_selection_df_from_kafka(spark_df)
+        if df:
+            transformed_df = transform_streaming_data(df)
 
-            # Create Snowflake connection options
-            session = create_snowflake_connection(spark_conn)
-
-            if session is not None:
-                logging.info("Streaming is being started...")
-
-                # Configure streaming to write to Snowflake
-                streaming_query = (
-                    selection_df.writeStream.format("snowflake")
-                    .option("checkpointLocation", "tmp/checkpoint")
-                    .option("dbtable", os.getenv("SNOWFLAKE_TABLE"))
-                    .options(**sfOptions)
-                    .start()
-                )
-
-                streaming_query.awaitTermination()
-            else:
-                logging.error("Failed to create Snowflake session.")
-        else:
-            logging.error("Failed to connect to Kafka.")
-    else:
-        logging.error("Failed to create Spark connection.")
+            # Print the streamed data to the console
+            write_to_file(transformed_df, "/opt/spark/output")
